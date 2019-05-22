3ee6908537feafc6802fe723c71f047a05d40b0d
audrasjb <[audrasjb@gmail.com]>
2019-05-22 07:34:08 +0200

    Initial Commit: Core function + Widget

--- wp-includes/widgets/class-wp-widget-data-request-form.php
+++ wp-includes/widgets/class-wp-widget-data-request-form.php
@@ -50,65 +50,17 @@ class WP_Widget_Data_Request extends WP_Widget {
 		if ( $title ) {
 			echo $args['before_title'] . $title . $args['after_title'];
 		}
-
-		// Enqueue CSS/JS
-		wp_enqueue_script( 'gdrf-public-scripts' );
-		wp_enqueue_style( 'gdrf-public-styles' );
-
-		// Captcha
-		$number_one = rand( 1, 9 );
-		$number_two = rand( 1, 9 );
-
 		?>
 		
-		<form action="<?php echo esc_url( admin_url( 'admin-ajax.php' ) ); ?>" method="post" class="widget_data_request_form">
-			<input type="hidden" name="action" value="data_request">
-			<input type="hidden" name="data-request-form_data_human_key" value="<?php echo $number_one . '000' . $number_two; ?>" />
-			<input type="hidden" name="data-request-form-nonce" value="<?php echo wp_create_nonce( 'data-request-form-nonce' ); ?>" />
-
 		<?php if ( ! empty( $description ) ) : ?>
 			<div class="data-request-form-field-description">
 				<?php echo apply_filters( 'the_content', $description ); ?>
 			</div>
 		<?php endif; ?>
 
-			<div class="data-request-form-field-action" role="radiogroup" aria-labelledby="data-request-form-radio-label">
-				<p class="data-request-form-radio-label">
-					<?php esc_html_e( 'Select your request:' ); ?>
-				</p>
-
-				<input class="data-request-form-type-input" type="radio" name="data-request-form-type-export" value="export_personal_data">
-				<label for="data-request-form-type-export" class="data-request-form-type-label">
-					<?php esc_html_e( 'Export Personal Data' ); ?>
-				</label>
-				<br />
-				<input class="data-request-form-type-input" type="radio" name="data-request-form-type" value="remove_personal_data">
-				<label for="data-request-form-type-remove" class="data-request-form-type-label">
-					<?php esc_html_e( 'Remove Personal Data' ); ?>
-				</label>
-			</div>
-
-			<p class="data-request-form-field-email">
-				<label for="data-request-form-email">
-					<?php esc_html_e( 'Your email address (required)' ); ?>
-				</label>
-				<input type="email" name="data-request-form-email" required />
-			</p>
-
-			<p class="data-request-form-field-human">
-				<label for="data-request-form-human">
-					<?php esc_html_e( 'Human verification (required):' ); ?>
-					<?php echo $number_one . ' + ' . $number_two . ' = ?'; ?>
-				</label>
-				<input type="text" name="data-request-form-human" required />
-			</p>
-
-			<p class="data-request-form-field-submit">
-				<input type="submit" value="<?php esc_html_e( 'Send request' ); ?>" />
-			</p>
-		</form>
-
 		<?php
+		echo wp_get_privacy_data_request_form();
+
 		echo $args['after_widget'];
 	}
 
